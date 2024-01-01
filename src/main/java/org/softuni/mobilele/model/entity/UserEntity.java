8f904f77ed9418791dcdce4b44170341746b1f58
Denis Cholakov <denisscholakov@gmail.com>
2024-01-01 22:35:11 +0200

    intial commit

---
+++ src/main/java/org/softuni/mobilele/model/entity/UserEntity.java
@@ -0,0 +1,63 @@
+package org.softuni.mobilele.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+    private String email;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private boolean isActive;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserEntity setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserEntity setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserEntity setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public UserEntity setActive(boolean active) {
+        isActive = active;
+        return this;
+    }
+}
---
+++ src/main/java/org/softuni/mobilele/service/UserService.java
@@ -0,0 +1,7 @@
+package org.softuni.mobilele.service;
+
+import org.softuni.mobilele.model.dto.UserRegistrationDto;
+
+public interface UserService {
+    void registerUser(UserRegistrationDto userRegistrationDto);
+}
