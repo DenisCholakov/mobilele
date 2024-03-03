a9f5a29503fc3f3dfbab2563b1b2f3fd946a31c8
Denis Cholakov <denisscholakov@gmail.com>
2024-03-03 19:01:39 +0200

    add some functionalities

--- src/main/java/org/softuni/mobilele/service/UserService.java
+++ src/main/java/org/softuni/mobilele/service/UserService.java
@@ -1,7 +1,12 @@
 package org.softuni.mobilele.service;
 
+import org.softuni.mobilele.model.dto.UserLoginDTO;
 import org.softuni.mobilele.model.dto.UserRegistrationDto;
 
 public interface UserService {
     void registerUser(UserRegistrationDto userRegistrationDto);
+
+    boolean loginUser(UserLoginDTO userLoginDTO);
+
+    void logoutUser();
 }
--- src/main/java/org/softuni/mobilele/service/impl/UserServiceImpl.java
+++ src/main/java/org/softuni/mobilele/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package org.softuni.mobilele.service.impl;
 
+import org.softuni.mobilele.model.dto.UserLoginDTO;
 import org.softuni.mobilele.model.dto.UserRegistrationDto;
 import org.softuni.mobilele.model.entity.UserEntity;
 import org.softuni.mobilele.repository.UserRepository;
 import org.softuni.mobilele.service.UserService;
+import org.softuni.mobilele.util.CurrentUser;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +14,12 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final CurrentUser currentUser;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, CurrentUser currentUser) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.currentUser = currentUser;
     }
     
     @Override
@@ -23,7 +27,39 @@ public class UserServiceImpl implements UserService {
         userRepository.save(map(userRegistrationDto));
     }
 
+    @Override
+    public boolean loginUser(UserLoginDTO userLoginDTO) {
+        var userEntity = userRepository
+                .findByEmail(userLoginDTO.email())
+                .orElse(null);
+        boolean loginSuccess = false;
+
+        if (userEntity != null) {
+            String encodedPassword = userEntity.getPassword();
+            String rawPassword = userLoginDTO.password();
+
+            loginSuccess = passwordEncoder.matches(rawPassword, encodedPassword);
+
+            if (loginSuccess) {
+                currentUser
+                    .setLogged(true)
+                    .setFirstName(userEntity.getFirstName())
+                    .setLastName(userEntity.getLastName());
+            } else {
+                currentUser.logout();
+            }
+        }
+
+        return loginSuccess;
+    }
+
+    @Override
+    public void logoutUser() {
+        currentUser.logout();
+    }
+
     private UserEntity map(UserRegistrationDto userRegistrationDto) {
+
         return new UserEntity()
                 .setActive(true)
                 .setFirstName(userRegistrationDto.firstName())
